ac5d1e048408e0d74ee02e4cbe9a60929603c2fe
YUANQTIE <yuanpanlilio@gmail.com>
2025-11-03 12:51:13 +0800

    Modified working training file for model

--- training.py
+++ training.py
@@ -1,6 +1,46 @@
 import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 import pandas
+import numpy
+
+def has_number(word):
+    for char in word:
+        if char.isdigit():
+            return True
+    return False
+
+def has_consecutive_character(word):
+    for i in range(1, len(word)):
+        if word[i] == word[i-1]:
+            return True
+    return False
+
+def has_nonfil_character(word):
+    word = word.lower()
+    for char in word:
+        if char == 'c' or char == 'f' or char == 'z' or char == 'j' or char == 'q' or char == 'v' or char == 'x':
+            return True
+    return False
+
+def get_c_to_v_ratio(word):
+    v_count = 1.0
+    c_count = 0.0
+    word = word.lower()
+    for char in word:
+        if char >= 'a' and char <= 'z':
+            if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+                v_count += 1
+            else:
+                c_count += 1
+    return c_count / v_count
+
+def is_numeric(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
 
 columns = pandas.read_csv('[CSINTSY]GRP2_MCO2_DataSet.csv')
 sentence_id = [int(i) for i in columns['sentence_id']]
@@ -93,7 +133,7 @@ train_data, validation_data, train_label, validation_label = train_test_split(tr
 print(len(train_data), len(test_data), len(validation_data))
 print(len(train_label), len(test_label), len(validation_label))
 
-#feature extraction
+# feature extraction
 # Feature 1: Word Length, longer usually means FIL
 # Feature 2: Has soft-sounding affixes, denoting ENG
 # Feature 3: Has hard-sounding affixes, denoting FIL
@@ -104,24 +144,100 @@ print(len(train_label), len(test_label), len(validation_label))
 # Feature 8: Presence of characters (c, f, z, j, q, v, x), denoting ENG
 # Feature 9: Repeated characters in a row, (arrow, mallet), denoting ENG (sometimes its UNK-NE)
 # Feature 10: Has numbers but within the word (A4, G6), denoting UNK-NE
-# Feature 11: Al capitalized characters, usually button mashing (HSAHAHDASHAH or FJEDJOISKLFJDLK) denoting UNK-EXPR
+# Feature 11: All capitalized characters, usually button mashing (HSAHAHDASHAH or FJEDJOISKLFJDLK) denoting UNK-EXPR
 # Feature 12: Higher average syllable length, usually denoting FIL
+# feature 13: n-grams
 
 features_matrix = []
 
-for data in train_data:
+for i in range(0, len(train_data)):
+
     #feature 1
-    features_matrix.append(len(data))  
+    f1 = len(train_data[i])
     #feature 2
     #feature 3
-    #feature 4  
-    #feature 5  
-    #feature 6  
-    #feature 7  
-    #feature 8  
-    #feature 9  
+    #feature 4
+
+
+    #feature 5
+
+    #checks if first character of word is capitalized, and checks if the current sentence id is between other sentence id's.
+    f5 = 0
+
+    if train_data[i][0].isupper() and (sentence_id[i] == sentence_id[i-1] or sentence_id[i] == sentence_id[i+1]):
+        f5 = 1
+
+    #feature 6
+
+    f6 = 0
+
+    if i < len(train_data) - 1:
+        if len(train_data[i]) == 3 and train_data[i].isupper() and train_data[i+1].isdigit() and 3 <= len(train_data[i+1]) <= 4:
+            f6 = 1
+
+    #feature 7
+
+    f7 = get_c_to_v_ratio(train_data[i])
+
+    #feature 8
+
+    f8 = 0
+    if has_nonfil_character(train_data[i]):
+        f8 = 1
+
+    #feature 9
+
+    f9 = 0
+    if has_consecutive_character(train_data[i]):
+        f9 = 1
+
     #feature 10  
+
+    f10 = 0
+    if has_number(train_data[i]):
+        f10 = 1
+
     #feature 11  
+    
+    f11 = 0
+    if train_data[i].isupper():
+        fil = 1
+
     #feature 12  
 
-print(train_data)
+    features_matrix.append([f1, f5, f6, f7, f8, f9, f10, f11])  
+
+x = numpy.array(features_matrix)
+y = numpy.array(train_label)
+
+clf = MultinomialNB()
+clf.fit(x, y)
+
+def get_features(word):
+    f1 = len(word)
+    f5 = 1 if word[0].isupper() else 0  # simpler for single word
+    f6 = 0  # can't check license plate with single word
+    f7 = get_c_to_v_ratio(word)
+    f8 = 1 if has_nonfil_character(word) else 0
+    f9 = 1 if has_consecutive_character(word) else 0
+    f10 = 1 if has_number(word) else 0
+    f11 = 1 if word.isupper() else 0
+    return [f1, f5, f6, f7, f8, f9, f10, f11]
+
+
+# === Predicting new words ===
+
+
+predictions = []
+for word in validation_data:
+    if is_numeric(word):
+        predictions.append("NUM")
+    elif not word.isalnum():
+        predictions.append("SYM")
+    else:
+        word_features = get_features(word)
+        word_features = numpy.array(word_features).reshape(1, -1)
+        predictions.append(clf.predict(word_features))
+
+for word, label in zip(validation_data, predictions):
+    print(word, "is to", label)
